724800f46b4353d0e9772fad8885ec697ca20874
jeanonyuro <jean.onyuro@students.scott.ac.ke>
2022-12-16 00:06:45 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - print to string
  * Return: 1
  */
 int main(void)
 {
-	fwrite(1, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n");
+	write(1, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n");
 	return (1);
 }
