4f4155192b1038bebe5f569f6848cd8ae437a7ed
jeanonyuro <jean.onyuro@students.scott.ac.ke>
2023-01-17 00:40:04 -0800

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,26 @@
+#ifndef PI_H
+
+#define PI_H
+
+#define PI 3.14159265359
+
+#endif
+
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int ain(void)
+
+{
+
+	printf("%s\n", __FILE__);
+
+	return (0);
+
+}
